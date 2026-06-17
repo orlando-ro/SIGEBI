e7d43dc603f0340aaea2461733f4ac44ea66d898
Juan Ramon De Los Santos Ramirez <20251102@itla.edu.do>
2026-06-17 16:59:55 -0400

    Configura Entity Framework y conexión a SQL Server

--- SIGEBI.Infrastructure/Persistence/SIGEBIDbContext.cs
+++ SIGEBI.Infrastructure/Persistence/SIGEBIDbContext.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+// using SIGEBI.Domain.Entities; //  esto se desmontara cuando se creen las entidades
+using Microsoft.EntityFrameworkCore;
+
 
 namespace SIGEBI.Infrastructure.Persistence
 {
-    internal class SIGEBIDbContext
-    {
+   public class SIGEBIDbContext : DbContext
+   {
+            // Este constructor es OBLIGATORIO.
+            // es lo que recibe el "Connection String" desde el Program.cs de la API
+            public SIGEBIDbContext(DbContextOptions<SIGEBIDbContext> options) : base(options)
+            {
+            }
+
+            // --- ZONA DE TABLAS (DbSets) ---
+            // van las tablas a medida que avancen en sus módulos.
+            // Ejemplo (no lo descomenten hasta que la clase Usuario exista):
+            // public DbSet<Usuario> Usuarios { get; set; }
+        }
     }
-}
+
+
+
