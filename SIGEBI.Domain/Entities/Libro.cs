03f9bea95a63394dd18604e757cfabaf94149cbf
orlando <orlandoefrain06@gmail.com>
2026-06-17 21:24:20 -0400

    feat: Crear entidates de modululo Catalogo Bibliografico
    Se crearon las entidades de Categoria y Libro, y se aggrego la relacion entre ellos, tambien se agrego logica de negocio (logica de prestamos) en la entidad de libro, que puede cambiar a estar totalment en los servicios en Application, pero por ahora se deja para tener una idea de rich domain.

--- SIGEBI.Domain/Entities/Libro.cs
+++ SIGEBI.Domain/Entities/Libro.cs
@@ -4,7 +4,41 @@ using System.Text;
 
 namespace SIGEBI.Domain.Entities
 {
-    internal class Libro
+    public class Libro
     {
+        // id y datos basicos
+        public string ISBN { get; set; } = string.Empty;
+        public string Titulo { get; set; } = string.Empty;
+        public string NombreAutor { get; set; } = string.Empty;
+        public int AnioPublicacion { get; set; }
+
+        // control de inventario
+        public int CopiasTotales { get; set; }
+        public int CopiasDisponibles { get; set; }
+
+        // llave foranea logica y navegacion solo para categoria
+        public int IdCategoria { get; set; }
+        public virtual Categoria? Categoria { get; set; }
+
+
+        // reglas de negocio en dominio, puede cambiar si decidimos hacer toda la logica de negocios en servivios
+
+        public bool PrestarCopia()
+        {
+            if (CopiasDisponibles > 0)
+            {
+                CopiasDisponibles--;
+                return true;
+            }
+            return false;
+        }
+
+        public void DevolverCopia()
+        {
+            if (CopiasDisponibles < CopiasTotales)
+            {
+                CopiasDisponibles++;
+            }
+        }
     }
 }
