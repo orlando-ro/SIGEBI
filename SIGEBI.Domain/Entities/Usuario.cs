dba436882217b3085feffe9f489afcfeb44c2a60
orlando <orlandoefrain06@gmail.com>
2026-06-17 20:53:16 -0400

    feat: crear feature aislado de modulo Usuario para modulo de Notificaciones
    se agrego este feature para que notificaciones tenga una relacion valida sin arrastrar todo el modulo de usuario y no se ensucien la rama de notificaciones con cambios de modulo de usuario, se agrego la clase usuario y la logica para que notificaciones funcione.
    
    se espera un merge conflict: al hacer merge de la rama feature/Notificaciones con develop, se espera un merge conflict en el archivo Usuario.cs, ya que se hicieron cambios en ese archivo para agregar la logica necesaria para que notificaciones funcione, y se espera que ya haya cambios en ese mismo archivo en develop, arrastrados desde feature/Usuario, por lo que se tendra que resolver el conflicto manualmente.

--- SIGEBI.Domain/Entities/Usuario.cs
+++ SIGEBI.Domain/Entities/Usuario.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace SIGEBI.Domain.Entities
 {
-    internal class Usuario
+    public abstract class Usuario
     {
+        // para hacer la relacion
+        public string IdUsuario { get; set; } = string.Empty;
+
+        // para que el feature funcione
+        public virtual ICollection<Notificacion> Notificaciones { get; set; } = new List<Notificacion>();
     }
 }
