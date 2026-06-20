5ff582ae962b4621be0cdc4fefcad541efa0968d
orlando <orlandoefrain06@gmail.com>
2026-06-19 20:50:33 -0400

    feat: crear repositorio de usuario y su interfaz
    se agrega la interfaz IUsuarios y el repositorio RepositorioUsuario para manejar las operaciones relacionadas con los usuarios. Esto incluye métodos para crear, leer, actualizar y eliminar usuarios, así como cualquier otra funcionalidad necesaria para la gestión de usuarios.

--- SIGEBI.Application/Interfaces/IUsuarios.cs
+++ SIGEBI.Application/Interfaces/IUsuarios.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using SIGEBI.Domain.Entities;
 
 namespace SIGEBI.Application.Interfaces
 {
-    internal interface IUsuarios
+    // hereda de ibaserepository
+    public interface IUsuarios : IBaseRepository<Usuario>
     {
+        // metodo para cargar al usuario con todas sus listas relacionadas
+        // necesario para penalizaciones en dominio
+        Task<Usuario?> ObtenerUsuarioConDetallesAsync(string idUsuario);
     }
 }
--- SIGEBI.Infrastructure/Persistence/Repositories/RepositorioUsuario.cs
+++ SIGEBI.Infrastructure/Persistence/Repositories/RepositorioUsuario.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SIGEBI.Application.Interfaces;
+using SIGEBI.Domain.Entities;
+using SIGEBI.Infrastructure.Persistence; 
 
-namespace SIGEBI.Infrastructure.Persistence.Repositories
+namespace SIGEBI.Infrastructure.Repositories
 {
-    internal class RepositorioUsuario
+    public class RepositorioUsuario : BaseRepository<Usuario>, IUsuarios
     {
+        // Inyectamos el DbContext y se lo pasamos a la clase base
+        public RepositorioUsuario(SIGEBIDbContext context) : base(context)
+        {
+        }
+
+        // se implementa metodo especifico
+        public async Task<Usuario?> ObtenerUsuarioConDetallesAsync(string idUsuario)
+        {
+            
+            return await _dbSet
+                .Include(u => u.Notificaciones)
+                .Include(u => u.Penalizaciones)
+                .FirstOrDefaultAsync(u => u.IdUsuario == idUsuario);
+        }
     }
 }
