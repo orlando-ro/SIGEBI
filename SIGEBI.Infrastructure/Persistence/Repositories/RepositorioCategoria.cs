dad050f4304231f078519fd495785901264d9fa6
orlando <orlandoefrain06@gmail.com>
2026-06-19 20:59:53 -0400

    feat: implementar repositorios e interfaces para modulo de catalogo bibliografico

--- SIGEBI.Infrastructure/Persistence/Repositories/RepositorioCategoria.cs
+++ SIGEBI.Infrastructure/Persistence/Repositories/RepositorioCategoria.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SIGEBI.Application.Interfaces;
+using SIGEBI.Domain.Entities;
+using SIGEBI.Infrastructure.Persistence;
 
-namespace SIGEBI.Infrastructure.Persistence.Repositories
+namespace SIGEBI.Infrastructure.Repositories
 {
-    internal class RepositorioCategoria
+    public class RepositorioCategoria : BaseRepository<Categoria>, IRepositorioCategoria
     {
+        public RepositorioCategoria(SIGEBIDbContext context) : base(context)
+        {
+        }
     }
 }
--- SIGEBI.Infrastructure/Persistence/Repositories/RepositorioLibro.cs
+++ SIGEBI.Infrastructure/Persistence/Repositories/RepositorioLibro.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SIGEBI.Application.Interfaces;
+using SIGEBI.Domain.Entities;
+using SIGEBI.Infrastructure.Persistence;
 
-namespace SIGEBI.Infrastructure.Persistence.Repositories
+namespace SIGEBI.Infrastructure.Repositories
 {
-    internal class RepositorioLibro
+    public class RepositorioLibro : BaseRepository<Libro>, IRepositorioLibro
     {
+        public RepositorioLibro(SIGEBIDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Libro?> ObtenerLibroConCategoriaAsync(string isbn)
+        {
+            //JOIN con la tabla Categorías
+            return await _dbSet
+                .Include(l => l.Categoria)
+                .FirstOrDefaultAsync(l => l.ISBN == isbn);
+        }
     }
 }
