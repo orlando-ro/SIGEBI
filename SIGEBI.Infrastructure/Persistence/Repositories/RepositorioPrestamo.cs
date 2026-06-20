c7556f781fa164d1a4b0a2e5c1dc3ba6ffdbfcd7
Juan Ramon De Los Santos Ramirez <20251102@itla.edu.do>
2026-06-20 01:10:27 -0400

    integrar patron repositorio generico en gestion de prestamos y solicitudes

--- SIGEBI.Infrastructure/Persistence/Repositories/RepositorioPrestamo.cs
+++ SIGEBI.Infrastructure/Persistence/Repositories/RepositorioPrestamo.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SIGEBI.Application.Interfaces;
+using SIGEBI.Domain.Entities;
+using SIGEBI.Infrastructure.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SIGEBI.Infrastructure.Persistence.Repositories
 {
-    internal class RepositorioPrestamo
+    internal class RepositorioPrestamo : BaseRepository<Prestamo>, IRepositorioPrestamo
     {
+        public RepositorioPrestamo(SIGEBIDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Prestamo>> ObtenerActivoPorUsuarioAsync(string idUsuario)
+        {
+            return await _dbSet
+                .Where(p => p.IdUsuario == idUsuario && p.Estado == "Activo").ToListAsync();
+        }
+
+        public async Task<Prestamo?> obtenerPrestamoConDetalleAsync(int id)
+        {
+            return await _dbSet
+               .Include(p => p.Libros)
+               .Include(p => p.Usuario)
+               .FirstOrDefaultAsync(p => p.IdPrestamo == id);
+        }
     }
 }
