bd1ebcb35e03abee1bccfda61ef609175cc16594
orlando <orlandoefrain06@gmail.com>
2026-06-29 04:10:25 -0400

    feat: desarrollar capa de negocio para notificaciones
    se agregan los métodos para enviar notificaciones a los usuarios, incluyendo la implementación de la interfaz IServicioNotificacion y la clase ServicioNotificaciones. Además, se actualiza el DTO NotificacionResponseDTO para reflejar los cambios necesarios en la respuesta de las notificaciones.

--- SIGEBI.Infrastructure/Services/ServicioNotificaciones.cs
+++ SIGEBI.Infrastructure/Services/ServicioNotificaciones.cs
@@ -1,10 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SIGEBI.Application.DTOs;
+using SIGEBI.Application.Interfaces;
 
 namespace SIGEBI.Infrastructure.Services
 {
-    internal class ServicioNotificaciones
+    public class ServicioNotificaciones : IServicioNotificacion
     {
+        private readonly IRepositorioNotificacion _repoNoti;
+
+        public ServicioNotificaciones(IRepositorioNotificacion repoNoti)
+        {
+            _repoNoti = repoNoti;
+        }
+
+        public async Task<IEnumerable<NotificacionResponseDTO>> ObtenerPendientesAsync(string idUsuario)
+        {
+            var pendientes = await _repoNoti.ObtenerNoLeidasPorUsuarioAsync(idUsuario);
+
+            return pendientes.Select(n => new NotificacionResponseDTO
+            {
+                Id = n.IdNotificacion,
+                Mensaje = n.Mensaje,
+                FechaEnvio = n.FechaEnvio,
+                Tipo = n.Tipo.ToString(),
+                Leida = n.Leida
+            });
+        }
+
+        public async Task MarcarComoLeidaAsync(int idNotificacion)
+        {
+            var notificacion = await _repoNoti.ObtenerPorIdAsync(idNotificacion);
+            if (notificacion != null && !notificacion.Leida)
+            {
+                notificacion.MarcarComoLeida();
+                await _repoNoti.ActualizarAsync(notificacion);
+            }
+        }
     }
 }
