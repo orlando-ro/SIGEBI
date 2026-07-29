8d433f0ee663464452894851b00bc49b524fed87
Juan Ramon De Los Santos Ramirez <20251102@itla.edu.do>
2026-07-29 13:14:23 -0400

    agregar el handler para validar los jwt de las peticiones http de cada usuario

---
+++ SIGEBI.AppWeb/Handler/JwtTokenHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Http;
+
+namespace SIGEBI.AppWeb.Handlers
+{
+    public class JwtTokenHandler : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public JwtTokenHandler(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // 1. Buscamos el token en la sesión del usuario actual
+            var token = _httpContextAccessor.HttpContext?.Session.GetString("TokenJwt");
+
+            // 2. Si existe, lo inyectamos automáticamente en la cabecera de la petición saliente
+            if (!string.IsNullOrEmpty(token))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            // 3. Dejamos que la petición continúe su viaje hacia la API
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ SIGEBI.AppWeb/Services/ServicioSolicitudApi.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http.Json;
+using SIGEBI.AppWeb.Models.Solicitudes;
+
+
+
+namespace SIGEBI.AppWeb.Services
+{
+    public class ServicioSolicitudApi
+    {
+        private readonly HttpClient _httpclient; // se usa para realizar solicitudes HTTP a la API
+        
+
+        public ServicioSolicitudApi(HttpClient httpClient, IHttpContextAccessor httpContextAccessor, IConfiguration configuration)
+        {
+            _httpclient = httpClient;
+            
+            var baseUrl = configuration["ApiSettings:BaseUrl"] ?? "http://localhost:7291/api";
+            _httpclient.BaseAddress = new Uri(baseUrl);
+        }
+
+
+        public async Task<SolicitudItemViewModel> CrearSolicitudAsync(List<string> isbnLibros) { 
+        
+            var playload = new { ISBNs = isbnLibros }; 
+
+
+
+        }
+    }
+}
